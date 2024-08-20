dfcdcd1c43ab0da59b2343894ac38dbbeadf3b8e
axmed97 <axmed.dev.13@gmail.com>
2024-08-20 17:55:57 +0400

    StateMachineCreated

--- SagaStateMachine.Service/Program.cs
+++ SagaStateMachine.Service/Program.cs
@@ -1,9 +1,21 @@
 using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using SagaStateMachine.Service.StateDbContext;
+using SagaStateMachine.Service.StateInstances;
+using SagaStateMachine.Service.StateMachines;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddMassTransit(configurator =>
 {
+    configurator.AddSagaStateMachine<OrderStateMachine, OrderStateInstance>()
+        .EntityFrameworkRepository(options =>
+        {
+            options.AddDbContext<DbContext, OrderStateDbContext>((provider, _builder) =>
+            {
+                _builder.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+            });
+        });
     configurator.UsingRabbitMq((ctx, cfg) =>
     {
         cfg.Host("amqps://ktzqjmzg:CtC9AkyO-89Pyg0h4ZEOafGNqlbVtI9i@puffin.rmq2.cloudamqp.com/ktzqjmzg");
