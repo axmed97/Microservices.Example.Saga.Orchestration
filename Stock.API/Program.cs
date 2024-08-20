1c810e670b41aa4e8d9398992438eb0dbb8817e2
axmed97 <axmed.dev.13@gmail.com>
2024-08-20 16:04:21 +0400

    Stock Complete

--- Stock.API/Program.cs
+++ Stock.API/Program.cs
@@ -1,4 +1,7 @@
 using MassTransit;
+using Stock.API.Models;
+using Stock.API.Services;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,22 @@ builder.Services.AddMassTransit(configurator =>
     });
 });
 
+builder.Services.AddSingleton<MongoDbService>();
+
 var app = builder.Build();
+
+using var scope = builder.Services.BuildServiceProvider().CreateScope();
+
+var mongoDbService = scope.ServiceProvider.GetRequiredService<MongoDbService>();
+
+if(!await(await mongoDbService.GetCollection<StockEntity>().FindAsync(x => true)).AnyAsync())
+{
+    mongoDbService.GetCollection<StockEntity>().InsertOne(new StockEntity { Count = 100, ProductId = 1 });
+    mongoDbService.GetCollection<StockEntity>().InsertOne(new StockEntity { Count = 200, ProductId = 2 });
+    mongoDbService.GetCollection<StockEntity>().InsertOne(new StockEntity { Count = 300, ProductId = 3 });
+    mongoDbService.GetCollection<StockEntity>().InsertOne(new StockEntity { Count = 400, ProductId = 4 });
+    mongoDbService.GetCollection<StockEntity>().InsertOne(new StockEntity { Count = 500, ProductId = 5 });
+}
+
 
 app.Run();
---
+++ Stock.API/Services/MongoDbService.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+
+namespace Stock.API.Services
+{
+    public class MongoDbService
+    {
+        private readonly IMongoDatabase mongoDatabase;
+        public MongoDbService(IConfiguration configuration)
+        {
+            MongoClient mongoClient = new(configuration.GetConnectionString("Default"));
+            mongoDatabase = mongoClient.GetDatabase("Default");
+        }
+
+        public IMongoCollection<T> GetCollection<T>() => mongoDatabase.GetCollection<T>(typeof(T).Name.ToLowerInvariant());
+    }
+}
